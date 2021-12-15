87ad29e03ab60d3117a8ab604f20b80dfb70df32
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-15 09:08:03 +0100

    Added MeetupDetail page and component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,10 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return <MeetupDetail
+    image="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/MJK_54041_Barocktr%C3%A4dg%C3%A5rden_%28Drottningholm%29.jpg/800px-MJK_54041_Barocktr%C3%A4dg%C3%A5rden_%28Drottningholm%29.jpg"
+    title="First Meetup" description="First meetup description" address="some street"
+  />
+}
+
+export default MeetupDetails;
