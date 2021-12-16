c296acf1a98945d5f3c340116e87facbe795d2ee
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-16 09:37:40 +0100

    added get meetups directly without API

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,5 @@
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A first meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/MJK_54041_Barocktr%C3%A4dg%C3%A5rden_%28Drottningholm%29.jpg/800px-MJK_54041_Barocktr%C3%A4dg%C3%A5rden_%28Drottningholm%29.jpg',
-    address: 'Some address',
-    description: 'This is the first meetup'
-  },
-  {
-    id: 'm2',
-    title: 'A second meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/MJK_54041_Barocktr%C3%A4dg%C3%A5rden_%28Drottningholm%29.jpg/800px-MJK_54041_Barocktr%C3%A4dg%C3%A5rden_%28Drottningholm%29.jpg',
-    address: 'Some address',
-    description: 'This is the first meetup'
-  }
-];
+import { MongoClient } from 'mongodb';
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups}/>;
@@ -38,9 +22,22 @@ function HomePage(props) {
 // next.js waits until the data load to render the page
 // you always needs to return a project as props
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://kronos:katsa999@cluster0.hrnez.mongodb.net/meetups?retryWrites=true&w=majority');
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map(meetup => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString()
+      }))
     },
     revalidate: 1 //the seconds next.js wait for revalidation and the pregenerated
     // pages will be replaced with the new ones
