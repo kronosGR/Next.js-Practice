0ad6fb5b1bcad0608eeb12f632d6edb9ab689524
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-16 09:20:17 +0100

    added mongodb and insert meetup to DB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,18 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      'mongodb+srv://kronos:katsa999@cluster0.hrnez.mongodb.net/meetups?retryWrites=true&w=majority');
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
