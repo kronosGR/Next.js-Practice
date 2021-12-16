de9ed1efa567bb8b65f8dea566398b5f6b6396d6
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-16 09:30:03 +0100

    added send POST request to API

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,23 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import { useRouter } from 'next/router';
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData){
-    console.log(enteredMeetupData)
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+    const data = await response.json();
+
+    router.push('/');
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+  return <NewMeetupForm onAddMeetup={addMeetupHandler}/>;
 }
 
-export default NewMeetupPage
+export default NewMeetupPage;
